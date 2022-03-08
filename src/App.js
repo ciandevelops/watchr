9f4d2f111f61cd5df7881bdd6a7e7bb0a3e763ad
Cian Butler <ciandevelops@gmail.com>
2022-03-07 22:37:10 -0500

    app working needs css

--- src/App.js
+++ src/App.js
@@ -3,33 +3,44 @@ import { BsSearch } from "react-icons/bs";
 import MovieCard from "./components/MovieCard";
 
 function App() {
-  useEffect(() => {
-    const API_URL = `http://www.omdbapi.com?apikey=${process.env.REACT_APP_API_KEY}&s=shrek`;
-    fetch(API_URL)
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-  }, []);
+  const API_URL = `http://www.omdbapi.com?apikey=${process.env.REACT_APP_API_KEY}`;
+  const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState("");
 
-  const movie = {
-    Title: "Shrek",
-    Year: "2001",
-    imdbID: "tt0126029",
-    Type: "movie",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BOGZhM2FhNTItODAzNi00YjA0LWEyN2UtNjJlYWQzYzU1MDg5L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
   };
+
+  useEffect(() => {
+    searchMovies("Shrek");
+  }, []);
 
   return (
     <div className="App">
       <h1>Watchr</h1>
 
       <div className="search">
-        <input type="text" placeholder="Search For Movie..." />
-        <BsSearch />
+        <input
+          type="text"
+          placeholder="Search For Movie..."
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        />
+        <button
+          onClick={() => {
+            searchMovies(search);
+          }}
+        >
+          <BsSearch />
+        </button>
       </div>
 
       <div className="container">
-        <MovieCard movie={movie} />
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.imdbID} />
+        ))}
       </div>
     </div>
   );
