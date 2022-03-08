301f8f367ab0cecd8a359ef297097815620ec4c1
Cian Butler <ciandevelops@gmail.com>
2022-03-07 21:39:17 -0500

    api working and displayed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { BsSearch } from "react-icons/bs";
 
 function App() {
   useEffect(() => {
@@ -8,9 +9,30 @@ function App() {
       .then((data) => console.log(data));
   }, []);
 
+  const movie = {
+    Title: "Shrek",
+    Year: "2001",
+    imdbID: "tt0126029",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BOGZhM2FhNTItODAzNi00YjA0LWEyN2UtNjJlYWQzYzU1MDg5L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+  };
+
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1>Watchr</h1>
+
+      <div className="search">
+        <input type="text" placeholder="Search For Movie..." />
+        <BsSearch />
+      </div>
+
+      <div className="container">
+        <h1>{movie.Title}</h1>
+        <h3>{movie.Year}</h3>
+        <p>{movie.Type}</p>
+        <img src={movie.Poster} alt="movie poster" />
+      </div>
     </div>
   );
 }
